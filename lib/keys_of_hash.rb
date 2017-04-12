e717a513b16c4aefad3fabeca1facac4c05754b1
Kyle Welton <kylewelt@gmail.com>
2017-04-12 16:57:47 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*values)
     # code goes here
+    results = []
+
+    values.each do |value|
+      if self.values.include?(value)
+        self.each do |key, word|
+          if word == value
+            results << key
+          end
+        end
+      end
+    end
+
+    results
   end
 end
